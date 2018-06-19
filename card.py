58e4faa1072596dce9ad16ee0c7ac6c130a18f59
Thomas Li <tli@e1z2r7p22.42.us.org>
2018-06-19 14:03:08 -0700

    Updated card.py

--- card.py
+++ card.py
@@ -6,23 +6,24 @@ class Card():
         self.suit=suit 
     def __str__(self):
         return "{0} of {1}".format(self.rank, self.suit)
-class Suit(): 
-    def __init__(self, suitname):
-        self.suitname=suitname
-    def __str__(self):
-        return str(self.suitname)
 class Deck():
+    def __init__(self):
+        self.deck=[Card(rank, suit) for rank in range(14) for suit in range(Diamonds, Spades, Hearts, Clubs)]
     def __str__(self):
         return [str(card) for card in self.deck]
     def pop(self):
         return self.deck.pop()
     def shuffle(self):
         return self.deck.shuffle()
-    def __init__(self):
-        self.deck=[Card(rank, suit) for rank in range(14) for suit in [Diamonds, Spades, Hearts, Clubs]   
-Diamonds=Suit('Diamonds')
-Spades=Suit('Spades')
-Hearts=Suit('Hearts')
-Clubs=Suit('Clubs')
+class Suit(): 
+    def __init__(self, suitname):
+        self.suitname=suitname
+    def __str__(self):
+        return str(self.suitname)
+    
+Diamonds=Suit('Diamonds', '♢')
+Spades=Suit('Spades', '♠')
+Hearts=Suit('Hearts', '♡')
+Clubs=Suit('Clubs', '♣')
 d=Deck()
 print(d)
